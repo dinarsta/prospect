8cc1a812d3134ec6873a80464d925eb418ad4291
Dina Rosita <rositadina816@gmail.com>
2022-12-09 16:14:03 +0700

    9

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,130 +1,123 @@
 @extends('layout.main')
 @section('content')
-<div class="container-xl px-4 mt-4">
-    <!-- Account page navigation-->
-    <div class="row">
-        <div class="col-xl-4">
-            <!-- Profile picture card-->
-            <div class="card mb-4 mb-xl-0">
-                <div class="card-header">Detail profile</div>
-                <div class="card-body text-center">
-                    <!-- Profile picture image-->
-                    <img class="img-account-profile rounded-circle mb-2" src="{{asset('template/images/faces/pp.jpg')}}"
-                        alt="">
-                    <!-- Profile picture help block-->
-                    <div class="small font-italic text-muted mb-4">
-                        <h3>{{Auth::user()->name}}</h3>
-                    </div>
-                    <div class="small font-italic text-muted mb-4">
-                        <h4>{{Auth::user()->email}}</h4>
-                    </div>
-                    <!-- Profile picture upload button-->
 
+
+<div class="row">
+    <div class="col-4">
+        <!-- Profile picture card-->
+        <div class="card mb-4 mb-xl-0">
+            <div class="card-header">Detail profile</div>
+            <div class="card-body">
+                <!-- Profile picture image-->
+               <div class="profile text-center">
+                 <img class="img-account-profile rounded-circle mb-2" src="{{asset('template/images/faces/pp.jpg')}}"
+                    alt="">
+                <!-- Profile picture help block-->
+                <div class="small font-italic text-muted mb-3">
+                    <h3>{{Auth::user()->name}}</h3>
                 </div>
-            </div>
-        </div>
-        <div class="col-xl-8">
-            <!-- Account details card-->
-            <div class="card mb-4">
-                <div class="card-header">Edit profile</div>
-                <div class="card-body">
-                    <form action="{{route('update', auth::user()->id)}}" method="post">
-                        @csrf
-                        @method('put')
-
-
-                        <!-- Form Row-->
-                        <div class=" mb-3">
-                            <!-- Form Group (first name)-->
-                            <label class="small mb-1" for="name">Full name</label>
-                            <input class="form-control" name="name" id="name" type="text"
-                                placeholder={{auth::user()->name}} required>
-                            <!-- Form Group (last name)-->
-                        </div>
-                        <div class="mb-3">
-                            <label class="small mb-1" for="inputEmailAddress">Email address</label>
-                            <input class="form-control" name="email" id="inputEmailAddress" type="email"
-                                placeholder={{auth::user()->email}} required>
-                        </div>
-                        <!-- Form Row-->
-                        <!-- Save changes button-->
-                        <button class="btn btn-primary" type="submit">Save changes</button>
-                    </form>
+                <div class="small font-italic text-muted mb-4">
+                    <h5>{{Auth::user()->email}}</h5>
                 </div>
+               </div>
+            <form action="{{route('update', auth::user()->id)}}" method="post">
+                    @csrf
+                    @method('put')
+                    <div class=" mb-3">
+                    </div>
+                    <!-- Form Group (first name)-->
+                    <label style="text-align:left" class="small mb-2 mt-2" for="name">Full name</label>
+                    <input class="form-control" name="name" id="name" type="text" placeholder={{auth::user()->name}}
+                        required>
+                    <div class="mb-3">
+                        <label class="small mb-2 mt-2" for="inputEmailAddress">Email address</label>
+                        <input class="form-control" name="email" id="inputEmailAddress" type="email"
+                            placeholder={{auth::user()->email}} required>
+                    </div>
+                    <button class="btn btn-primary" type="submit">Save changes</button>
+            
+
+                </form>
             </div>
         </div>
     </div>
-</div>
-<style>
-    body {
-        margin-top: 20px;
-        background-color: #f2f6fc;
-        color: #69707a;
-    }
 
-    .img-account-profile {
-        height: 10rem;
-    }
+    <style>
+        
+        body {
+            margin-top: 20px;
+            background-color: #f2f6fc;
+            color: #69707a;
+        }
 
-    .rounded-circle {
-        border-radius: 50% !important;
-    }
+        .img-account-profile {
+            height: 10rem;
+        }
 
-    .card {
-        box-shadow: 0 0.15rem 1.75rem 0 rgb(33 40 50 / 15%);
-    }
+        .rounded-circle {
+            border-radius: 50% !important;
+        }
 
-    .card .card-header {
-        font-weight: 500;
-    }
+        .card {
+            box-shadow: 0 0.15rem 1.75rem 0 rgb(33 40 50 / 15%);
 
-    .card-header:first-child {
-        border-radius: 0.35rem 0.35rem 0 0;
-    }
+        }
 
-    .card-header {
-        padding: 1rem 1.35rem;
-        margin-bottom: 0;
-        background-color: rgba(33, 40, 50, 0.03);
-        border-bottom: 1px solid rgba(33, 40, 50, 0.125);
-    }
+        .card .card-header {
+            font-weight: 500;
+        }
 
-    .form-control,
-    .dataTable-input {
-        display: block;
-        width: 100%;
-        padding: 0.875rem 1.125rem;
-        font-size: 0.875rem;
-        font-weight: 400;
-        line-height: 1;
-        color: #69707a;
-        background-color: #fff;
-        background-clip: padding-box;
-        border: 1px solid #c5ccd6;
-        -webkit-appearance: none;
-        -moz-appearance: none;
-        appearance: none;
-        border-radius: 0.35rem;
-        transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
-    }
+        .card-header:first-child {
+            border-radius: 0.35rem 0.35rem 0 0;
+        }
 
-    .nav-borders .nav-link.active {
-        color: #0061f2;
-        border-bottom-color: #0061f2;
-    }
+        .card-header {
+            padding: 1rem 1.35rem;
+            margin-bottom: 0;
+            background-color: rgba(33, 40, 50, 0.03);
+            border-bottom: 1px solid rgba(33, 40, 50, 0.125);
+        }
 
-    .nav-borders .nav-link {
-        color: #69707a;
-        border-bottom-width: 0.125rem;
-        border-bottom-style: solid;
-        border-bottom-color: transparent;
-        padding-top: 0.5rem;
-        padding-bottom: 0.5rem;
-        padding-left: 0;
-        padding-right: 0;
-        margin-left: 1rem;
-        margin-right: 1rem;
-    }
+        .form-control,
+        .dataTable-input {
+            display: block;
+            width: 100%;
+            padding: 0.875rem 1.125rem;
+            font-size: 0.875rem;
+            font-weight: 400;
+            line-height: 1;
+            color: #69707a;
+            background-color: #fff;
+            background-clip: padding-box;
+            border: 1px solid #c5ccd6;
+            -webkit-appearance: none;
+            -moz-appearance: none;
+            appearance: none;
+            border-radius: 0.35rem;
+            transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+        }
 
-</style>
-@endsection
+        .nav-borders .nav-link.active {
+            color: #0061f2;
+            border-bottom-color: #0061f2;
+        }
+
+        .nav-borders .nav-link {
+            color: #69707a;
+            border-bottom-width: 0.125rem;
+            border-bottom-style: solid;
+            border-bottom-color: transparent;
+            padding-top: 0.5rem;
+            padding-bottom: 0.5rem;
+            padding-left: 0;
+            padding-right: 0;
+            margin-left: 1rem;
+            margin-right: 1rem;
+        }
+
+        .row {
+            justify-content: center;
+        }
+
+    </style>
+    @endsection
